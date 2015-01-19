bedea9a894201954003aebd1a339bf5d60af2bfc
Griffin Yourick <gryphon92@gmail.com>
2015-01-19 14:40:45 -0500

    Use bower-rails

--- app/assets/javascripts/flashes.js
+++ app/assets/javascripts/flashes.js
@@ -1,10 +1,9 @@
-$(document).ready(function () {
+$(document).ready(function() {
   var $flash = $('.flash');
-  var $close = $('.flash-close');
 
-  $flash.on('click', function () {
+  $flash.on('click', function() {
     var $elem = $(this);
-    $elem.fadeOut(500, function () {
+    $elem.fadeOut(500, function() {
       $elem.remove();
     });
   });
--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
-  var menuToggle = $('#js-mobile-menu').unbind();
-  $('#js-nav-menu').removeClass("show");
+  var $menuToggle = $('#js-mobile-menu'),
+      $menu = $('#js-nav-menu');
 
-  menuToggle.on('click', function(e) {
+  $menuToggle.unbind();
+  $menu.removeClass('show');
+
+  $menuToggle.on('click', function(e) {
     e.preventDefault();
-    $('#js-nav-menu').slideToggle(function(){
-      if($('#js-nav-menu').is(':hidden')) {
-        $('#js-nav-menu').removeAttr('style');
+    $menu.slideToggle(function() {
+      if ($menu.is(':hidden')) {
+        $menu.removeAttr('style');
       }
     });
   });
