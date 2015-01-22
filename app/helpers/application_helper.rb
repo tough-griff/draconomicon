7fea3a4be9bfb57d3e4e14a0d5e014f6338e008e
Griffin Yourick <gryphon92@gmail.com>
2015-01-22 01:13:52 -0500

    Upgrade bitters, navbar re-write [WIP]

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,10 @@
 module ApplicationHelper
-  # Returns a link wrapped in a list element with the classes "nav-link" and
-  # "active" if the link points to the current page.
-  def nav_link_to(name = nil, options = nil, html_options = nil, &block)
-    content_tag :li, class: nav_link_class(url_for(options)) do
-      link_to(name, options, html_options, &block)
+  # Returns a link wrapped in a list element.
+  # If the link points to the current page, the "active" class is added.
+  def nav_link_to(name, url)
+    html_opts = { class: "active" } if current_page?(url)
+    content_tag :li do
+      link_to(name, url, html_opts)
     end
-  end
-
-  def nav_link_class(url)
-    return "nav-link active" if current_page?(url)
-    "nav-link"
   end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,27 +1,22 @@
 require "rails_helper"
 
 RSpec.describe ApplicationHelper do
-  before { allow(self).to receive(:current_page?).and_return(false) }
-
   describe "#nav_link_to" do
-    it "returns the correct link" do
-      expect(self).to receive(:link_to).with("text", "url", class: "class")
-      nav_link_to("text", "url", class: "class")
-    end
-
     context "current_page? false" do
-      it "wraps without the 'active' class" do
-        expect(self).to receive(:content_tag).with(:li, class: "nav-link")
-        nav_link_to("text", "url", class: "class")
+      before { allow(self).to receive(:current_page?).and_return(false) }
+
+      it "does not add the 'active' class" do
+        expect(self).to receive(:link_to).with("text", "url", nil)
+        nav_link_to("text", "url")
       end
     end
 
     context "current_page? true" do
       before { allow(self).to receive(:current_page?).and_return(true) }
 
-      it "wraps with the 'active' class" do
-        expect(self).to receive(:content_tag).with(:li, class: "nav-link active")
-        nav_link_to("text", "url", class: "class")
+      it "adds the 'active' class" do
+        expect(self).to receive(:link_to).with("text", "url", class: "active")
+        nav_link_to("text", "url")
       end
     end
   end
