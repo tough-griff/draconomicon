689252d60c214ae9f28a9603071c2db491332933
Griffin Yourick <gryphon92@gmail.com>
2015-02-04 14:43:45 -0500

    Change the name to simply 'Draconomicon'

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   # ActionMailer
-  config.action_mailer.default_url_options = { host: "the-draconomicon.dev" }
+  config.action_mailer.default_url_options = { host: "draconomicon.dev" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { address: "localhost", port: 1025 }
   config.action_mailer.raise_delivery_errors = true
