dba1fd79caa0387a7a889b0b394967e2761e5986
Griffin Yourick <gryphon92@gmail.com>
2015-04-29 20:51:46 -0400

    Use the active_link_to gem instead of rolling my own method

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ ruby "2.2.2"
 gem "rails", "4.2.1"
 
 gem "airbrake"
+gem "active_link_to"
 gem "bourbon"
 gem "devise"
 gem "font-awesome-sass"
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,28 +1,4 @@
 require "rails_helper"
 
 RSpec.describe ApplicationHelper do
-  describe "#nav_link_to" do
-    context "current_page? false" do
-      before { allow(self).to receive(:current_page?).and_return(false) }
-
-      it "does not add the 'active' class" do
-        expect(self).to receive(:link_to).with("text", "url", method: :delete)
-        nav_link_to("text", "url", method: :delete)
-      end
-
-      it "handles a block" do
-        expect(self).to receive(:link_to).with("text", "url", nil) { "block content" }
-        nav_link_to("text", "url") { "block content" }
-      end
-    end
-
-    context "current_page? true" do
-      before { allow(self).to receive(:current_page?).and_return(true) }
-
-      it "adds the 'active' class" do
-        expect(self).to receive(:link_to).with("text", "url", nil)
-        nav_link_to("text", "url")
-      end
-    end
-  end
 end
